1e20f5fb1179593ee763c8fc928da4ecd38f82be
Shannon Burke <shayray601@gmail.com>
2020-04-15 00:36:22 -0500

    i fixed the app somehow

--- app/src/main/java/com/example/scaryalarmclock/SplashScreen.java
+++ app/src/main/java/com/example/scaryalarmclock/SplashScreen.java
@@ -11,7 +11,6 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
 
             @Override
